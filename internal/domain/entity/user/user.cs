ebb29ee5fe871ae153316bf2f600c53bef97cfe9
root <root@DESKTOP-BCRQVPF>
2024-11-18 12:30:26 +0700

    file managment

---
+++ internal/domain/entity/user/user.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Domain.Entity;
+
+public class UserEntity
+{
+    public Guid ID { get; set; }
+    public string Name { get; set; }
+    public string HashPassword { get; set; }
+
+    public UserEntity(in string name, in string password)
+    {
+        ID = Guid.NewGuid();
+        Name = name;
+        HashPassword = Hash(password); // realize hashing password
+    }
+
+    private string Hash(string input)
+    {
+        return input;
+    }
+}
---
+++ internal/infrastucture/storage/user/user_storage.cs
@@ -0,0 +1,41 @@
+﻿using Npgsql;
+using System;
+
+namespace Infrastucture.Storage;
+
+public class UserStorage : IDisposable
+{
+    private NpgsqlConnection _connection;
+
+    public UserStorage(string connectionString)
+    {
+        _connection = new NpgsqlConnection(connectionString);
+
+        try
+        {
+            _connection.Open();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"{ex}");
+            // ADD Log
+            throw;
+        }
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        _connection?.Dispose();
+    }
+
+    ~UserStorage()
+    {
+        Dispose(false);
+    }
+}
